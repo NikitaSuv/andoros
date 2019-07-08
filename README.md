f320e3b5424a4f3c0ab1a1e4ce625b2a3afcc4c3
Nikita <nikitossuv@gmail.com>
2019-07-08 21:30:28 +0300

    andoros

---
+++ README.md
@@ -0,0 +1,8 @@
+# start-html
+Start HTML Template.
+# How start
+1. cd start-html-template
+2. npm run init
+3. npm run serve
+# For build to Prod
+1. npm run build
---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Section: jQuery
+ */
+//@@include('general/global/global.js')
+//@@include('general/ui/ui.js')
+//@@include('general/components/components.js')
+//@@include('general/sections/sections.js')
+
+
+$(function () {
+	$('.slider').slick({
+		dots: false,
+	});
+});
